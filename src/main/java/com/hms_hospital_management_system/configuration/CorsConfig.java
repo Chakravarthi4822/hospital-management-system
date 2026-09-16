a1e50254b40b755a6689552e970d70700129eb1d
Chakravarthi <chakri4822@gmail.com>
2026-09-16 06:39:33 +0000

    Allow Netlify frontend in CORS

--- src/main/java/com/hms_hospital_management_system/configuration/CorsConfig.java
+++ src/main/java/com/hms_hospital_management_system/configuration/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                                 "http://127.0.0.1:5500",
                                 "http://localhost:5500",
                                 "http://127.0.0.1:5501",
-                                "http://localhost:5501"
+                                "http://localhost:5501",
+                                "https://6aaa383704a1118e75495434--harmonious-travesseiro-96edc2.netlify.app"
                         )
                         .allowedMethods(
                                 "GET",
